4e16d7a6de243cdee027771702e183d268ffd986
Dark Samurai <keshav223.keshav@gmail.com>
2025-04-21 11:24:08 +0530

    Update login_user.js

--- api/login_user.js
+++ api/login_user.js
@@ -3,26 +3,7 @@ import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore/li
 
 // Firebase config (replace with your own)
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBZz1eZfi4P5NQlWww9ucZOfph6aFn9tgo",
-  authDomain: "todolist-b8869.firebaseapp.com",
-  projectId: "todolist-b8869",
-  storageBucket: "todolist-b8869.firebasestorage.app",
-  messagingSenderId: "412884284217",
-  appId: "1:412884284217:web:120b59ca908cc29c34eee8",
-  measurementId: "G-WY5M376CG6"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
